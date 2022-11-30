5582c6e165fd144583b071f11aa156055b2bfb66
al3tiby <al3tiby2003@gmail.com>
2022-11-30 16:24:53 +0300

    Making and coding Blog app

--- project_management/settings.py
+++ project_management/settings.py
@@ -58,7 +58,7 @@ ROOT_URLCONF = 'project_management.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
