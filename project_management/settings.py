40f849d41a302b1a8c96e9bd787c1bcc04a74aaa
al3tiby <al3tiby2003@gmail.com>
2022-12-07 23:46:48 +0300

    ready for deployment

--- project_management/settings.py
+++ project_management/settings.py
@@ -172,6 +172,10 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
 
+STATICFILES_ROOT = [
+    os.path.join(BASE_DIR, 'static'),
+]
+
 MEDIA_URL = '/images/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'static/img/uplouded')
 
