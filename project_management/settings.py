404e06d2b8dca88b7ee9c2314090c9367436d0ad
al3tiby <al3tiby2003@gmail.com>
2022-12-08 11:35:41 +0300

    Fixing some producion problems

--- project_management/settings.py
+++ project_management/settings.py
@@ -30,7 +30,7 @@ environ.Env.read_env()
 SECRET_KEY = env('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['housstates.herokuapp.com', '127.0.0.1']
 
