c04d215172e0df9271d737718672bdb4d2476755
al3tiby <al3tiby2003@gmail.com>
2022-12-08 14:46:16 +0300

    Fixing some producion problems

--- project_management/settings.py
+++ project_management/settings.py
@@ -171,7 +171,7 @@ STATIC_ROOT = BASE_DIR / "staticfiles"
 STATIC_URL = "static/"
 
 # Enable WhiteNoise's GZip compression of static assets.
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+# STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 
 STATICFILES_DIRS = [
