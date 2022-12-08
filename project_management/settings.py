759b2eeeb62bf3ecd3d27447527273ddbe253969
al3tiby <al3tiby2003@gmail.com>
2022-12-08 13:18:43 +0300

    Fixing some producion problems

--- project_management/settings.py
+++ project_management/settings.py
@@ -172,7 +172,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/images/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'static/img/uplouded')
